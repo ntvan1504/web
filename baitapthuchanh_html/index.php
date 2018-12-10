f57634d8da5121daaf3a64fdc671ca64b9ff71ca
Thanh Van <thanhvan.jvb@gmail.com>
2018-12-10 16:31:34 +0700

    thay doi duong dan

--- baitapthuchanh_html/index.php
+++ baitapthuchanh_html/index.php
@@ -4,7 +4,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-include "apps/bootstrap.php";
+include ".\Apps\bootstrap.php";
 
 //$a= new Apps_Libs_DbConnection();
 
